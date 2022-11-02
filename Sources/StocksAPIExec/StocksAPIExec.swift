ca5ead29862e5a01f475507411d71d2844ddcef3
Edgar Francisco López Enríquez <109615404+elopezLumston@users.noreply.github.com>
2022-11-01 22:28:32 -0600

    [FIX] shortname variable in Ticker Model

--- Sources/StocksAPIExec/StocksAPIExec.swift
+++ Sources/StocksAPIExec/StocksAPIExec.swift
@@ -14,15 +14,15 @@ struct StocksAPIExec {
     
     static func main() async {
         do {
-            let quotes = try await stocksAPI.fetchQuotes(symbols: "")
-            print(quotes)
+            //let quotes = try await stocksAPI.fetchQuotes(symbols: "")
+            //print(quotes)
             
             let tickers = try await stocksAPI.searchTickers(query: "tesla")
             print(tickers)
             
-            if let chart = try await stocksAPI.fetchChartData(symbol: "AAPL", range: .oneDay) {
-                print(chart)
-            }
+            //if let chart = try await stocksAPI.fetchChartData(symbol: "AAPL", range: .oneDay) {
+            //    print(chart)
+            //}
         } catch {
             print(error.localizedDescription)
         }
